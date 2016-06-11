1c157923567058b6183866c7aadf99ce7c48a366
Daniel Brown <csengineer13@gmail.com>
2016-06-10 21:14:50 -0400

    wiring up click events

--- CodeKata/Scripts/app.js
+++ CodeKata/Scripts/app.js
@@ -1,6 +1,40 @@
 ﻿$(function() {
 
+
+	vm.init();
+
     // kickstart jQuery DataTable
     $('#example').DataTable();
 
 });
+
+
+
+var vm = {};
+
+vm.init = function()
+{
+	bindClickEvents();
+};
+
+var bindClickEvents = function()
+{
+	//open the lateral panel
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.modal').addClass('is-open');
+		$('body').addClass('modal-open');
+	});
+	//close the lateral panel
+	$('.modal').on('click', function(event){
+		if( $(event.target).is('.modal') || $(event.target).is('.modal-close') ) { 
+			
+			$('.modal').addClass('animate-out');
+			$('.modal').removeClass('is-open');
+			
+			$('body').removeClass('modal-open');
+			event.preventDefault();
+		}
+	});
+
+};
