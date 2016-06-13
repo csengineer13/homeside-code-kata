524acce2866ca2859f66b38fbdad5ec5cd2c886a
Daniel Brown <csengineer13@gmail.com>
2016-06-12 22:10:53 -0400

    Adding new DTO for select2 user mapping

---
+++ CodeKata.ViewModel/DTO/User/UserSearchDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeKata.ViewModel.DTO
+{
+    public class UserSearchDto
+    {
+        public int id { get; set; } // Has to be lowercase for select2
+        public string Name { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
